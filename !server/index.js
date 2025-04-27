c467f65a9e4162c872c4ad0881ea16d0182f926e
xQudo <alyaqdhan690s@gmail.com>
2025-04-27 23:42:29 +0400

    first commit

---
+++ !server/index.js
@@ -0,0 +1,11 @@
+import express, { response } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect('mongodb+srv://admin:admin@cluster.uuf2say.mongodb.net/pos?retryWrites=true&w=majority&appName=Cluster');
+
+app.listen(4000, () => console.log('server is connected to port 4000'));
