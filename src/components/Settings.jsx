3840c1dbb17d4ba9e5239541ca4c1c381707909f
xQudo <alyaqdhan690s@gmail.com>
2025-05-20 23:26:16 +0400

    deleted counter

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Counter from './Counter';
 
 function Settings() {
   return (
     <div className='content'>
       <h1>Settings</h1>
-      <Counter initialCount={5} />
     </div>
   )
 }
