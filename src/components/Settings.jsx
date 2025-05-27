6b317e99a4a4a9e2fbdbe84534e830b414c0d3e6
xQudo <alyaqdhan690s@gmail.com>
2025-05-27 18:14:44 +0400

    added outlets and reorganized

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -3,8 +3,9 @@ import { Link, Route, Routes, useLocation } from 'react-router-dom'
 import { Nav, NavLink as NL } from 'reactstrap'
 import SettingsIndex from "./settings/index";
 import Categories from "./settings/Categories";
-import Customers from "./settings/Customers";
 import Suppliers from "./settings/Suppliers";
+import Customers from "./settings/Customers";
+import Outlets from "./settings/Outlets";
 import Payments from "./settings/Payments";
 
 function Settings() {
@@ -16,26 +17,30 @@ function Settings() {
         <NL active={location.pathname === '/settings'}>
           <Link className='link' to='/settings'>Settings</Link>
         </NL>
-        <NL active={location.pathname === '/settings/suppliers'}>
-          <Link className='link' to='/settings/suppliers'>Suppliers</Link>
-        </NL>
-        <NL active={location.pathname === '/settings/payments'}>
-          <Link className='link' to='/settings/payments'>Payments</Link>
-        </NL>
         <NL active={location.pathname === '/settings/categories'}>
           <Link className='link' to='/settings/categories'>Categories</Link>
+        </NL>
+        <NL active={location.pathname === '/settings/suppliers'}>
+          <Link className='link' to='/settings/suppliers'>Suppliers</Link>
         </NL>
         <NL active={location.pathname === '/settings/customers'}>
           <Link className='link' to='/settings/customers'>Customers</Link>
         </NL>
+        <NL active={location.pathname === '/settings/payments'}>
+          <Link className='link' to='/settings/payments'>Payments</Link>
+        </NL>
+        <NL active={location.pathname === '/settings/outlets'}>
+          <Link className='link' to='/settings/outlets'>Outlets</Link>
+        </NL>
       </Nav>
 
       <Routes>
-        <Route path="" element={<SettingsIndex />} />
-        <Route path="categories" element={<Categories />} />
-        <Route path="customers" element={<Customers />} />
-        <Route path="suppliers" element={<Suppliers />} />
-        <Route path="Payments" element={<Payments />} />
+        <Route path="/" element={<SettingsIndex />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/suppliers" element={<Suppliers />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/Payments" element={<Payments />} />
+        <Route path="/Outlets" element={<Outlets />} />
       </Routes>
     </div>
   )
