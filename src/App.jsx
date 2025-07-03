5259c6a09bd23863e4da8de6031f9a132d6857af
xQudo <alyaqdhan690s@gmail.com>
2025-07-03 06:05:59 +0400

    added dashboard empty page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
 import Products from "./components/Products";
 import Users from "./components/Users";
@@ -22,6 +23,7 @@ function App() {
       <ToastContainer theme="colored" position="bottom-center" pauseOnFocusLoss={false} draggable />
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="/products" element={<ProtectedRoute><Products /></ProtectedRoute>} />
         <Route path="/users" element={<ProtectedRoute><Users /></ProtectedRoute>} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { NavLink, useNavigate } from 'react-router-dom';
 import { Nav, Button, Spinner } from 'reactstrap';
 import { getUsers, logout } from '../features/UserSlice';
-import { MdLogout } from 'react-icons/md';
+import { MdDashboard, MdLogout } from 'react-icons/md';
 import { FaCog, FaHome, FaUsers } from "react-icons/fa";
 import { BiSolidPackage } from "react-icons/bi";
 
@@ -24,18 +24,16 @@ function Header() {
 
   return (
     <Nav className='Nav'>
+      <NavLink to="/dashboard" className={dynamicLink}><MdDashboard/> Dashboard</NavLink>
       <NavLink to="/" className={dynamicLink}><FaHome/> Home</NavLink>
-
       {
         (user.permissions.products.read == true) &&
         <NavLink to="/products" className={dynamicLink}><BiSolidPackage/> Products</NavLink>
       }
-
       {
         (user.permissions.users.read == true) &&
         <NavLink to="/users" className={dynamicLink}><FaUsers/> Users ({userList.length})</NavLink>
       }
-
       <NavLink to="/settings" className={dynamicLink}><FaCog/> Settings</NavLink>
 
       <span className='userInfo'>
